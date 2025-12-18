fa61064f4fd05a3e9281d6ad4afaafa33b0967cb
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-12-18 23:38:25 +0100

    Creation of the JS code, plus some interesting functions

---
+++ Auth API/securityWebSocket/src/test/java/com/securityWebSocket/securityWebSocket/SecurityWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityWebSocket.securityWebSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Pagina Web/js/app.js
+++ Pagina Web/js/app.js
@@ -1,19 +1,40 @@
 // First we have to declare the API where we can validate the user's login 
 // based on the data inside the database 
-const API_URL = 'http://localhost:9085'; // This part ain't going to change in a near future 
+const API_URL = 'http://localhost:8089'; // This part ain't going to change in a near future 
 const LOGIN_ENDPOINT = '${API_URL}/auth/login'; // Basically the path of the login inside of the AuthController inside of the API
 
 
 
 // Declaration of all the variables in the HTML 
-let email = document.getElementById('email');
-let password = document.getElementById('password'); 
-let loginBtn = document.getElementById('loginBtn'); 
-let togglePassword = document.getElementById('togglePassword'); 
+const email = document.getElementById('email');
+const password = document.getElementById('password'); 
+const loginBtn = document.getElementById('loginBtn'); 
+const togglePassword = document.getElementById('togglePassword'); 
+const passwordInput = document.getElementById('password'); 
+
 
 // Function to show the password or to keep it sealed 
-togglePasswordBtn.addEventListener('click', function() {
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    this.innerHTML = type === 'password' ? '👁️' : '🙈';
-});
+if(togglePassword){
+    togglePassword.addEventListener('click', function() {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        this.innerHTML = type === 'password' ? '👁️' : '🙈';
+    });
+}else{
+    console.log('Data from the button ' + email); 
+    console.log('No button created!'); 
+}
+
+
+// Function to enter the web after a propper validation 
+loginBtn.addEventListener('click' , function() {
+    let passwordValidation = password.value; 
+    if(password != null ||
+        passwordValidation.length < 3 || 
+        passwordValidation.length > 15 
+    ){
+        console.log('Access denied to the web, Invalid password setting!'); 
+    }else{
+        console.log('Access granted, welcome!')
+    }
+}); 
