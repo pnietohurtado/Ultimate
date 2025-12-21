3b9f7721bb6842caa3fb932adedaeb31110848fe
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-12-21 11:27:50 +0100

    Finish the register screen

---
+++ Pagina Web/js/register.js
@@ -0,0 +1,46 @@
+const LOGIN_ENDPOINT = 'http://localhost:9085/auth/register'; // Register ENDPOINT 
+
+const nombre = document.getElementById('nombre'); 
+const apellido = document.getElementById('apellido'); 
+const email = document.getElementById('email'); 
+const username = document.getElementById('username'); 
+const password = document.getElementById('password'); 
+const loginBtn = document.getElementById('loginBtn'); 
+
+// The login botton in order to send to the API all the information 
+loginBtn.addEventListener('click', function(){
+    register(nombre.value, apellido.value, email.value, username.value, password.value); 
+}); 
+
+async function register(nombre, apellido, email, username, password){
+    try{
+        
+        const credentials = {
+            firstName: nombre, 
+            lastName: apellido, 
+            email: email, 
+            username: username, 
+            password: password
+        }
+
+        const response = await fetch(`${LOGIN_ENDPOINT}`, {
+            method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json', 
+                        'Accept': 'application/json' 
+                    },
+                    body: JSON.stringify(credentials),
+                    credentials: 'include'
+        }); 
+
+        if(response.ok){
+            console.log('User added to the database!'); 
+        }else{
+            console.log('Could not connect to the database!' ); 
+        }
+
+    }catch(error){
+        console.error(error); 
+    }
+
+}
