b78c58de82005934ef6c1bfe58bf6c7fb5d1cfb9
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2025-12-26 12:00:04 +0100

    Adding the chat sidebar

--- Pagina Web/js/websocket.js
+++ Pagina Web/js/websocket.js
@@ -1,6 +1,9 @@
 const sendBtn = document.getElementById('sendMessage'); 
 const textField = document.getElementById('messageInput'); 
 const messageContainer = document.getElementById('message-container'); 
+const chat01 = document.getElementById('chat01')
+const chat02 = document.getElementById('chat02')
+const chat03 = document.getElementById('chat03')
 
 sendBtn.addEventListener('click', function(){
     const messageDiv = document.createElement('div'); 
@@ -9,7 +12,7 @@ sendBtn.addEventListener('click', function(){
 
     messageDiv.textContent = textField.value; 
     messageDiv.classList.add('message-bubble', 'user'); 
-    console.log(messageDiv.className)
+    console.log(messageDiv.className.split(" ").at(1)) // In order to get the first class name
     messageContainer.appendChild(messageDiv); 
 
     textField.value = ''; 
@@ -34,5 +37,29 @@ textField.addEventListener('keydown', function(e){
     
 }); 
 
+
+chat01.addEventListener('click', function(){
+    if(chat01.className.split(" ").at(1) === 'active'){
+        console.log('You are alredy selected'); 
+    }else{
+        chat01.classList.add(chat01.className.split(" ").at(0), 'active'); 
+    }
+})
+
+chat02.addEventListener('click', function(){
+    if(chat02.className.split(" ").at(1) === 'active'){
+        console.log('You are alredy selected'); 
+    }else{
+        chat02.classList.add(chat01.className.split(" ").at(0), 'active'); 
+    }
+})
+
+chat03.addEventListener('click', function(){
+    if(chat03.className.split(" ").at(1) === 'active'){
+        console.log('You are alredy selected'); 
+    }else{
+        chat03.classList.add(chat01.className.split(" ").at(0), 'active'); 
+    }
+})
  
 
