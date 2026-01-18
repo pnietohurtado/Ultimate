40bef72db6b60e4953d656850e344a1f2c99a64b
pnietohurtado <pnietohurtado03@gmail.com>
2026-01-18 15:59:01 +0100

    Adding the mongodb functions

--- Pagina Web/js/MongoDBCode/mongo.js
+++ Pagina Web/js/MongoDBCode/mongo.js
@@ -1,13 +1,42 @@
 import {getConnection} from "../connectionMongo/connectionMongoDB.js"; 
 
-const getLanguage = async () => {
+const getId = async () => {
     try{
 
+        const db = await getConnection(); 
+        
+
+    }catch(error){
+        console.error(error); 
+    }
+}
+
+const getConvData = async () => { // It should return the id of the conversation 
+    try{
+
+        /*
         const database = await getConnection(); 
         const languages = await database.collection("languages").find().toArray(); 
 
         console.table(languages); 
         console.log("Languages listed!"); 
+        */
+
+        const db = await getConnection(); 
+        const id = null; 
+        //const id = await db.collection("[El nombre de la colección es el id de la conversación]").find().toArray(); 
+        return id; 
+
+    }catch(error){
+        console.error(error); 
+    }
+}
+
+// Function to add one more message to the count list 
+const addingNumMessage = async () => {
+    try{
+
+
 
     }catch(error){
         console.error(error); 
