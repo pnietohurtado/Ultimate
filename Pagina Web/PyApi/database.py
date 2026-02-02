237cad66af8f583bd496637e45a7f783662efdde
pnietohurtado <pnietohurtado03@gmail.com>
2026-02-02 17:37:58 +0100

    Creation of the model of py

--- Pagina Web/PyApi/database.py
+++ Pagina Web/PyApi/database.py
@@ -1,10 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from models import Client
+from models import Message
 from bson import ObjectId
 
-client = AsyncIOMotorClient('mongodb://localhost:27017/')
-database = client.FinalDataBase
-collection = database.clients
+message = AsyncIOMotorClient('mongodb://localhost:27017/')
+database = message.FinalDataBase
+collection = database.messages
 
 async def initializeDB():
     count = await collection.count_documents({})
@@ -19,27 +19,27 @@ async def initializeDB():
     else:
         print(f"Base de datos ya tiene {count} documentos")
 
-async def get_one_client(id: int):
-    client_doc = await collection.find_one({'_id':id})
-    if client_doc:
-        return Client(**client_doc)
+async def get_one_message(id: int):
+    message_doc = await collection.find_one({'_id':id})
+    if message_doc:
+        return Message(**message_doc)
     return None
 
-async def get_all_client():
-    clients = []
+async def get_all_messages():
+    messages = []
     cursor = collection.find({})
     async for document in cursor:
         # Asegurar que el documento tiene todos los campos requeridos
         if 'name' in document and 'surname' in document and 'ticket' in document:
-            clients.append(Client(**document))
-    return clients
+            messages.append(Message(**document))
+    return messages
 
-async def create_client(client_data: dict):
-    if '_id' not in client_data:
+async def send_message(message: dict):
+    if '_id' not in message:
         # Generar un nuevo ID si no se proporciona
-        last_client = await collection.find_one(sort=[('_id', -1)])
-        client_data['_id'] = last_client['_id'] + 1 if last_client else 1
+        last_message = await collection.find_one(sort=[('_id', -1)])
+        message['_id'] = last_message['_id'] + 1 if last_message else 1
     
-    await collection.insert_one(client_data)
-    created_client = await collection.find_one({'_id': client_data['_id']})
-    return Client(**created_client) if created_client else None
+    await collection.insert_one(message)
+    created_client = await collection.find_one({'_id': message['_id']})
+    return Message(**created_client) if created_client else None
--- Pagina Web/PyApi/main.py
+++ Pagina Web/PyApi/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from models import Message
 
 app = FastAPI()
 
