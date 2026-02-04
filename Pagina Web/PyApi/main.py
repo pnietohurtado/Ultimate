3ac8d82eed6e62a07109280d6ede611fa7ab01ec
pnietohurtado <pnietohurtado03@gmail.com>
2026-02-04 09:12:34 +0100

    adding new functions in the API in order to read and to add the message

--- Pagina Web/PyApi/main.py
+++ Pagina Web/PyApi/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from models import Message
-from database import initializeDB,add_message
+from database import initializeDB,add_message, get_all_messages
 from models import WhoIs
 
 app = FastAPI()
@@ -10,3 +10,15 @@ async def welcome():
     await initializeDB()
     await add_message(WhoIs.USER.value, 'Mensaje desde mongoDB')
     return {'message': 'Hello!'}
+
+
+@app.get('/api/sendmessage/{who}/{message}')
+async def sendMessage(who: WhoIs, message:str): 
+    await add_message(who, message); 
+    return {'message': 'Everything send correctly'}
+
+
+@app.get('/api/getmessages')
+async def getAllMessages(): 
+    await get_all_messages() 
+    return {'message': 'Getting all the messages'}
